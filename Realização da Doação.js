ce985eb014c9a4bb3587598028547e090363e2b0
SidneyGabriel227 <163361061+SidneyGabriel227@users.noreply.github.com>
2024-06-10 21:21:18 -0300

    Update Realização da Doação.js

--- Realização da Doação.js
+++ Realização da Doação.js
@@ -27,7 +27,8 @@ function cadastroProduto() {
 
     localStorage.setItem("dadosProduto", JSON.stringify(dados));
     alert("Doação Realizada");
-     window.location.href = "Solicitação de produtos efetuada.html";
+
+    window.location.href = "Solicitação de produtos efetuada.html";
     // Limpar os campos
     Produto.value = "";
     Quantidade.value = "";
