92d12fd7b6c8a47fcfc4bc179cf82341124a35a9
KamillyMacedo <160599457+KamillyMacedo@users.noreply.github.com>
2024-06-11 22:00:09 -0300

    Update solicitardoacao.js

--- solicitardoacao.js
+++ solicitardoacao.js
@@ -25,7 +25,7 @@ function cadastroProduto() {
 
     localStorage.setItem("dadosProduto", JSON.stringify(dados));
     alert("Doação Realizada");
-    window.location.href = "Solicitação de produtos efetuada.html";
+    window.location.href="Solicitação de produtos efetuada.html";
     // Limpar os campos
     Produto.value = "";
     Quantidade.value = "";
